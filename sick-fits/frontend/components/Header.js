06432a1d4a13a3aaee20ca547ea00ff62b7a595f
jamesbyrne89 <mail@jamestbyrne.com>
2019-02-03 22:29:56 +0000

    Add Header component and theme

---
+++ sick-fits/frontend/components/Header.js
@@ -0,0 +1,58 @@
+import Link from 'next/link';
+import Nav from './Nav';
+import styled from 'styled-components';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    padding: 0.5rem 1rem;
+    background: ${props => props.theme.red};
+    color: #fff;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+`;
+
+const StyledHeader = styled.header`
+  .bar {
+    border-bottom: 10px solid ${props => props.theme.black};
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+    @media (max-width: ${props => props.theme.bpLarge}) {
+      grid-template-columns: 1fr;
+      justify-content: center;
+    }
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: solid 1px ${props => props.theme.lightGrey};
+  }
+`;
+
+const Header = () => (
+  <StyledHeader>
+    <div className="bar">
+      <Logo>
+        <Link href="">
+          <a>Sick Fits</a>
+        </Link>
+      </Logo>
+      <Nav />
+    </div>
+    <div className="sub-bar">
+      <span>Search</span>
+    </div>
+    <div>
+      <span>Cart</span>
+    </div>
+  </StyledHeader>
+);
+
+export default Header;
--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,7 +1,40 @@
 import React, { Component } from 'react';
+import Meta from './Meta';
+import Header from './Header';
+import styled, { ThemeProvider } from 'styled-components';
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+  bpLarge: '1300px'
+};
+
+const StyledPage = styled.div`
+  background: ${props => props.theme.offWhite};
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.section`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2em;
+`;
 
 export default class Page extends Component {
   render() {
-    return <main>{this.props.children}</main>;
+    return (
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
+    );
   }
 }
