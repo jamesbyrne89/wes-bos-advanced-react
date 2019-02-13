d30e6c0b63891a08c385a95f632b9902a43e25fb
jamesbyrne89 <mail@jamestbyrne.com>
2019-02-13 23:26:15 +0000

    Latest progress - does not compile

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -4,15 +4,18 @@ import { ApolloProvider } from 'react-apollo';
 import withData from '../lib/withData';
 
 class StoreApp extends App {
-  static async getInitialProps({ Component, context }) {
-    const pageProps = Component.getInitialProps ? await Component.getInitialProps(context);
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
     // this exposes the query to the user
-    pageProps.query = context.query;
-    return { pageProps }
+    pageProps.query = ctx.query;
+    return { pageProps };
   }
-  
 
   render() {
+    console.log(this.props.apollo);
     const { Component, apollo, pageProps } = this.props;
     return (
       <Container>
